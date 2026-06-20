95bf7a5a5ca33663eb14e6067728af6469dcbf9c
Jung-YoonB <amazetong@naver.com>
2026-06-20 19:55:00 +0900

    [CodeUp 문제풀이_java] 1074.java_기초-반복실행구조

---
+++ java/Main/Basic12/1074.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+ public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+
+  int num = sc.nextInt();
+  
+  while (num > 0) {
+   System.out.println(num);
+   
+   num--;
+  }
+  
+
+  sc.close();
+ }
+
+}
