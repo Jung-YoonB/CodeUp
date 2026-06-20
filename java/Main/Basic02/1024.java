78611987c23e97809b3f5f335c62d7937ae91dea
Jung-YoonB <amazetong@naver.com>
2026-06-20 17:34:59 +0900

    [CodeUp 문제풀이_java] 1024.java_기초-입출력

---
+++ java/Main/Basic02/1024.java
@@ -0,0 +1,27 @@
+"import java.util.Scanner;
+
+public class Main {
+
+ public static void main(String[] args) {
+
+  Scanner sc = new Scanner(System.in);
+  
+  String word = sc.next(); 
+  
+  /* while 문으로도 작성
+  int i = 0;
+  while (i < word.length()) {
+   System.out.println(""'"" + word.charAt(i)+ ""'"");
+   i++;
+  }
+  */
+  
+  int i;
+  for (i = 0; i < word.length(); i++) {
+   System.out.printf(""'%c'\n"", word.charAt(i));
+  }
+  
+  sc.close();
+ }
+
+}"
