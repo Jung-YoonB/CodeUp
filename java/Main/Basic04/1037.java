b8eafc03b7068fe16f4d7ece9ea26c4a47bc3d5c
Jung-YoonB <amazetong@naver.com>
2026-06-20 19:06:11 +0900

    [CodeUp 문제풀이_java] 1037.java_기초-출력변환

---
+++ java/Main/Basic04/1037.java
@@ -0,0 +1,18 @@
+"import java.util.Scanner;
+
+public class Main {
+
+ public static void main(String[] args) {
+
+  Scanner sc = new Scanner(System.in);
+  
+  int num = sc.nextInt();
+  
+  char ch = (char)num;
+  
+  System.out.println(ch);
+  
+  sc.close();
+ }
+
+}"
