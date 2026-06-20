01e3a688d6885316256861bbd092c700cf5e3e9e
Jung-YoonB <amazetong@naver.com>
2026-06-20 17:26:09 +0900

    [CodeUp 문제풀이_java] 1008.java_기초-출력
    java 는 1008문항 CodeUp에서 결과 확인 불가.

---
+++ java/Main/Basic01/1008.java
@@ -0,0 +1,12 @@
+"// 1008 은 참고용, 자바는 사이트에서 불가능하다고함...
+public class Main {
+
+ public static void main(String[] args) {
+  
+  System.out.printf(""\u250C\u252C\u2510\n"");
+        System.out.printf(""\u251C\u253C\u2524\n"");
+        System.out.printf(""\u2514\u2534\u2518"");
+      
+ }
+
+}"
