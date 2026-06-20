565799e3d0188e3bbc9ac5e670de65bc663ab81e
Jung-YoonB <amazetong@naver.com>
2026-06-20 17:37:30 +0900

    [CodeUp 문제풀이_java] 1030.java_기초-데이터형

---
+++ java/Main/Basic03/1030.java
@@ -0,0 +1,17 @@
+"// 1028과 동일
+import java.util.Scanner;
+
+public class Main {
+
+ public static void main(String[] args) {
+
+  Scanner sc = new Scanner(System.in);
+  
+  long num = sc.nextLong(); 
+  
+  System.out.println(num);
+  
+  sc.close();
+ }
+
+}"
