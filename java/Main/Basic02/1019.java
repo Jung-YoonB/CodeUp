ecc0aa8e48102016cd1773b4574c08b7293bfaa9
Jung-YoonB <amazetong@naver.com>
2026-06-20 17:32:31 +0900

    [CodeUp 문제풀이_java] 1019.java_기초-입출력

---
+++ java/Main/Basic02/1019.java
@@ -0,0 +1,20 @@
+"import java.util.Scanner;
+
+public class Main {
+
+ public static void main(String[] args) {
+
+  Scanner sc = new Scanner(System.in);
+  
+  String[] date = sc.next().split(""\\.""); 
+  
+  int year = Integer.valueOf(date[0]);
+  int month = Integer.valueOf(date[1]);
+  int day = Integer.valueOf(date[2]);
+    
+  System.out.printf(""%04d.%02d.%02d"", year, month, day);
+
+  sc.close();
+ }
+
+}"
