10077a75e655c5392b239a4bfef7ad33cefc2ca6
Jung-YoonB <amazetong@naver.com>
2026-06-20 19:38:07 +0900

    따옴표 제거

--- java/Main/Basic08/1058.java
+++ java/Main/Basic08/1058.java
@@ -1,4 +1,5 @@
-"import java.util.Scanner;
+import java.util.Scanner;
+
 public class Main {
 
  public static void main(String[] args) {
@@ -12,4 +13,4 @@ public class Main {
   sc.close();
  }
 
-}"
+}
