42cff24888b31e1c72dfc68875872cd7b3fe88ee
Jung-YoonB <amazetong@naver.com>
2026-06-20 17:38:51 +0900

    [CodeUp 문제풀이_java] 1032.java_기초-출력변환

---
+++ java/Main/Basic04/1032.java
@@ -0,0 +1,17 @@
+"import java.util.Scanner;
+
+public class Main {
+
+ public static void main(String[] args) {
+
+  Scanner sc = new Scanner(System.in);
+  
+  int num = sc.nextInt(); 
+  
+  // %x : 16진수 출력
+  System.out.printf(""%x"", num);
+  
+  sc.close();
+ }
+
+}"
