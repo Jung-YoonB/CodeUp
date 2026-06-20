b72aa5d01359db53c66d1246526cdd07be65e85e
Jung-YoonB <amazetong@naver.com>
2026-06-20 17:29:44 +0900

    [CodeUp 문제풀이_java] 1012.java_기초-입출력

---
+++ java/Main/Basic02/1012.java
@@ -0,0 +1,15 @@
+"import java.util.Scanner;
+
+public class Main {
+
+ public static void main(String[] args) {
+
+  Scanner sc = new Scanner(System.in);
+  
+  float fNum = sc.nextFloat();  
+  System.out.printf(""%f"", fNum);
+  
+  sc.close();
+ }
+
+}"
