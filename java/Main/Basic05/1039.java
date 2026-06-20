0d60669adbf14a076eeea0da54c0ec7a5cb4f396
Jung-YoonB <amazetong@naver.com>
2026-06-20 19:07:12 +0900

    [CodeUp 문제풀이_java] 1039.java_기초-산술연산

---
+++ java/Main/Basic05/1039.java
@@ -0,0 +1,16 @@
+"// 1038과 동일
+import java.util.Scanner;
+public class Main {
+
+ public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  
+  long num1 = sc.nextLong();
+  long num2 = sc.nextLong();
+  
+  System.out.println(num1 + num2);
+  
+  sc.close();
+ }
+
+}"
