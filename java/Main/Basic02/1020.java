d029b2b4d0d9a12462f926b19ec51927bec3cb3c
Jung-YoonB <amazetong@naver.com>
2026-06-20 17:32:56 +0900

    [CodeUp 문제풀이_java] 1020.java_기초-입출력

---
+++ java/Main/Basic02/1020.java
@@ -0,0 +1,16 @@
+"import java.util.Scanner;
+
+public class Main {
+
+ public static void main(String[] args) {
+
+  Scanner sc = new Scanner(System.in);
+  
+  String[] rNum = sc.next().split(""-""); 
+        
+  System.out.println(rNum[0] + rNum[1]);
+
+  sc.close();
+ }
+
+}"
