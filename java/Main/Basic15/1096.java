d6bfe4263ae47a60b9c9ca841dbc13d74a76f841
Jung-YoonB <amazetong@naver.com>
2026-06-21 11:29:53 +0900

    항상 된 for 문 출력 연습 추가, 변수 초기화때 계산을 완료하도록 수정

--- java/Main/Basic15/1096.java
+++ java/Main/Basic15/1096.java
@@ -10,25 +10,31 @@ public class Main {
 		int whiteCount = sc.nextInt();
 		
 		for (int set = 0; set < whiteCount; set++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
+			int x = sc.nextInt() - 1;
+			int y = sc.nextInt() - 1;
 			// 입력 좌표는 1 부터 받음
 			// 인덱스는 0 이므로 -1 처리
 			// 여러번 입력 받아도 어차피 돌은 한번만 놓으니까 그냥 1
-			board[x - 1][y - 1] = 1;
+			board[x][y] = 1;
 		}
 		
-		for (int row = 0; row < board.length; row++) {
-						
-			for (int i = 0; i < board[row].length; i++) {
-												
-				System.out.print(board[row][i] + " ");
-				
-			}
-						
+		for (int row = 0; row < board.length; row++) {				
+			for (int i = 0; i < board[row].length; i++) {												
+				System.out.print(board[row][i] + " ");				
+			}						
 			System.out.println();
 		}
 						
+		// 항상된 for문 출력
+		/*
+		for (int[] row : board) {
+		 
+			for (int value : row) {
+				System.out.print(value + " ");
+			}
+			System.out.println();
+		}				
+		*/
 		
 		sc.close();
 	}
